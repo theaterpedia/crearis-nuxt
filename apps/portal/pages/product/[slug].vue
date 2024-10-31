60714ba326bb29ef5b55fe81d55cac5c6b3bc567
Hans <hans.doenitz@dasei.eu>
2024-10-31 09:13:59 +0100

    last tweaks and tests

---
+++ apps/portal/pages/product/[slug].vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtLayout :breadcrumbs="breadcrumbs" name="default">
+    <SectionContainer>
+      <NuxtErrorBoundary>
+        <div>Product</div>
+      </NuxtErrorBoundary>
+    </SectionContainer>
+  </NuxtLayout>
+  <!-- ContentDoc / -->
+</template>
