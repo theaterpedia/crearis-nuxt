ccd4aa62e0bc906d8fe51bb72f77d1af56aa8fb3
Hans <hans.doenitz@dasei.eu>
2024-11-08 10:46:16 +0100

    update password-input

--- components/ui/form/PasswordInput.vue
+++ components/ui/form/PasswordInput.vue
@@ -1,15 +1,16 @@
 <template>
   <SfInput :type="isPasswordVisible ? 'text' : 'password'">
     <template #suffix>
-      <button @click="isPasswordVisible = !isPasswordVisible" type="button">
-        <component :is="isPasswordVisible ? SfIconVisibility : SfIconVisibilityOff" />
-        <component :is="isPasswordVisible ? SfIconVisibility : SfIconVisibilityOff" />
+      <button type="button" @click="isPasswordVisible = !isPasswordVisible">
+        <component :is="isPasswordVisible ? SfIconVisibilityOff : SfIconVisibility" />
       </button>
     </template>
   </SfInput>
 </template>
 
 <script lang="ts" setup>
-import { SfIconsSfIconVisibility, SfIconsSfIconVisibilityOff } from '#build/components'
-const isPasswordVisible = ref<boolean>()
+import { ref } from 'vue';
+import { SfInput, SfIconVisibility, SfIconVisibilityOff } from '#components';
+
+const isPasswordVisible = ref<boolean>();
 </script>
