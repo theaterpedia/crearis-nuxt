315dce121d681fc902c60ab403fe0597dbfeb96e
Hans <hans.doenitz@dasei.eu>
2024-11-11 17:23:46 +0100

    Pruvious Section prepared

---
+++ packages/theme/.playground/components/pru/Section.vue
@@ -0,0 +1,38 @@
+<template>
+  <Section overlap>
+    <Container style="margin-top:-52px" >
+        <Heading :is="is" style="max-width:600px; margin-left:-44px; padding-left: 44px" :content="heading" class="bg-primary-400 py-2" >
+
+        </Heading>
+    </Container>
+    <Container class="mt-4" >
+      <slot>
+        <div v-if="content" v-html="content"></div>
+      </slot>
+    </Container>
+  </Section>
+</template>
+
+<script lang="ts" setup>
+import { Container, Section } from '@crearis/ui'
+
+const props = defineProps({
+  /**
+   * The heading tag to render.
+   *
+   * @default 'h2'
+   */
+  is: {
+    type: [Object, String] as PropType<'h1' | 'h2' | 'h3' | 'span' | 'li'>,
+    default: 'h2',
+  },
+  heading: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: false,
+  },  
+})
+</script>
--- packages/theme/.playground/pages/konferenz.vue
+++ packages/theme/.playground/pages/konferenz.vue
@@ -1,8 +1,52 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    <SectionContainer></SectionContainer>
+  <div class="bg-default">
+    <SectionContainer background="muted" overlap>
+
+    </SectionContainer>
+    <PruSection heading="**Headline Headline** Subline Subline">
+      <Prose>
+          <p>
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+          </p>
+          <p>
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+          </p>
+          <p>
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+          </p>                                
+      </Prose>
+    </PruSection>
+    <PruSection background="muted" heading="**Headline Headline** Subline Subline">
+      <Prose>
+          <p>
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+          </p>
+          <p>
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+          </p>
+          <p>
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+              Testeintrag Testeintrag
+          </p>                                
+      </Prose>
+    </PruSection>    
     <PlLayouts />
   </div>
 </template>
