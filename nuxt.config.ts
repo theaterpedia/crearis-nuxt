5ded3239869310eb62b0ab267ae06a2ac1c3b56e
Hans <hans.doenitz@dasei.eu>
2024-11-11 08:38:32 +0100

    fix wrong font-names, fix extend

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-10-30',
-  extends: ['./packages/sfui'],
+  extends: ['./packages/theme'],
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/google-fonts',
