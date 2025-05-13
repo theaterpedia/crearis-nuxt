a5b2d9f40e79a55810eed68565a50fe7fe7d1849
Hans <hans.doenitz@dasei.eu>
2025-05-13 13:00:01 +0200

    draft: Theming-Block > allows Theme-Config via Pruvious

---
+++ blocks/ThemeGallery.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>{{ title }}</h2>
+  <ThemeConfigurator background="muted"></ThemeConfigurator>
+</template>
+
+<script lang="ts" setup>
+import { defineBlock, textField } from '#pruvious'
+
+defineBlock({
+  icon: 'Pencil',
+  label: 'B: ThemeSwitch',
+})
+
+defineProps({
+  title: textField({
+    placeholder: 'Heading: _ID_ overline **HEADLINE** subline',
+    label: 'Heading (optional)',
+    required: false,
+  }),
+})
+
+</script>
